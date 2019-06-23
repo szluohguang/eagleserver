351565ec7f9ddc3dfdc5128152273a1c88bea578
szluohguang <luohguang@sina.cn>
2019-06-23 11:40:15 +0800

    23

--- eagleserver/src/main/java/com/eagleluo/eagleserver/model/TodoService.java
+++ eagleserver/src/main/java/com/eagleluo/eagleserver/model/TodoService.java
@@ -5,7 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 import com.eagleluo.eagleserver.base.TodoItemBase;
 
+import org.slf4j.LoggerFactory;
+
 public class TodoService {
+
+    private static org.slf4j.Logger logger = LoggerFactory.getLogger(TodoService.class);
 
     public List<TodoItemBase> todolist = new ArrayList<>();
         
@@ -20,7 +24,12 @@ public class TodoService {
         item = new TodoItemBase();
         item.setText( "月度汇报整理" );
         todolist.add( item );
-    
+        
+        item = new TodoItemBase();
+        item.setText( "工作规划整理" );
+        todolist.add( item );
+
+        logger.info( todolist.toString() );
         return todolist;
     }
 
